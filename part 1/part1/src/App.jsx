1a1fb10690a4f8792a15113cfed96b69305168b9
noiqum <onurcoskun99@gmail.com>
2023-09-16 08:32:37 +0300

    exercise 1.4

--- part 1/part1/src/App.jsx
+++ part 1/part1/src/App.jsx
@@ -3,27 +3,32 @@ import Header from "./components/Header/Header";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+  function getTotalExercise(partList) {
+    let total = 0;
+    partList.forEach((element) => {
+      total = total + element.exercises;
+    });
+    return total;
+  }
   return (
     <div>
       <Header course={course}></Header>
       <Content />
-      <p>
-        Number of exercises{" "}
-        {part1.exercises + part2.exercises + part3.exercises}
-      </p>
+      <p>Number of exercises {getTotalExercise(parts)}</p>
     </div>
   );
 };
