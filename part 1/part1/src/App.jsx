8a52af5c2d88d72a90e084ada9f3f03736ebe4ab
noiqum <onurcoskun99@gmail.com>
2023-09-17 16:21:25 +0300

    exercise 1.12

--- part 1/part1/src/App.jsx
+++ part 1/part1/src/App.jsx
@@ -1,36 +1,26 @@
-import Header from "./components/Header/Header";
 import { useState } from "react";
-import Statistics from "./components/Statistics/Statistics";
-import { FeedbackBtn } from "./components/FeedbackBtn/FeedbackBtn";
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0);
 
-  const goodBtnHandler = () => {
-    setGood(good + 1);
+  const btnHandler = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
   };
-  const neutralBtnHandler = () => {
-    setNeutral(neutral + 1);
-  };
-
-  const badBtnHandler = () => {
-    setBad(bad + 1);
-  };
-
   return (
     <div>
-      <Header course="give feedback"></Header>
-      <div>
-        <FeedbackBtn name="good" clickHandler={goodBtnHandler}></FeedbackBtn>
-        <FeedbackBtn
-          name="neutral"
-          clickHandler={neutralBtnHandler}
-        ></FeedbackBtn>
-        <FeedbackBtn name="bad" clickHandler={badBtnHandler}></FeedbackBtn>
-      </div>
-      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={btnHandler}>next anecdote</button>
     </div>
   );
 };
