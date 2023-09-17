17d5301b74213ec6c7415159bedfaf26e3ceaf92
noiqum <onurcoskun99@gmail.com>
2023-09-17 14:48:47 +0300

    exercise 1.6

--- part 1/part1/src/App.jsx
+++ part 1/part1/src/App.jsx
@@ -1,31 +1,35 @@
-import Content from "./components/Content/Content";
 import Header from "./components/Header/Header";
-import Total from "./components/Total/Total";
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodBtnHandler = () => {
+    setGood(good + 1);
+  };
+  const neutralBtnHandler = () => {
+    setNeutral(neutral + 1);
   };
 
+  const badBtnHandler = () => {
+    setBad(bad + 1);
+  };
   return (
     <div>
-      <Header course={course.name}></Header>
-      <Content parts={course.parts} />
-      <Total list={course.parts}></Total>
+      <Header course="give feedback"></Header>
+      <div>
+        <button onClick={goodBtnHandler}>good</button>
+        <button onClick={neutralBtnHandler}>neutral</button>
+        <button onClick={badBtnHandler}>bad</button>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h2>Statistics</h2>
+        <span>{`good ${good}`}</span>
+        <span>{`neutral ${neutral}`}</span>
+        <span>{`bad ${bad}`}</span>
+      </div>
     </div>
   );
 };
