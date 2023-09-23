7b9ea94d32fdc987fed7728d3d617f87254081b4
onur  coskun <onur.coskun@wunder.is>
2023-09-23 22:17:13 +0300

    exercise 2.6

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -1,56 +1,40 @@
-import Course from "../src/components/Course/Course";
+import { useState } from "react";
 
 const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
+  const newNameHandler = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+  };
+  const addNewName = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
   return (
     <div>
-      {courses.map((course) => {
-        return <Course key={course.id} course={course} />;
-      })}{" "}
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName} onChange={newNameHandler} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return (
+          <p key={person.name}>
+            <span>{person.name}</span>
+          </p>
+        );
+      })}
     </div>
   );
 };
