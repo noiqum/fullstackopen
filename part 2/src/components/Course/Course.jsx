8354c8eba1d21c15b5c94a7ecc90b1f7b5e872a0
onur  coskun <onur.coskun@wunder.is>
2023-09-23 21:51:49 +0300

    exercise 2.2

--- part 2/src/components/Course/Course.jsx
+++ part 2/src/components/Course/Course.jsx
@@ -1,12 +1,21 @@
 import PropTypes from "prop-types";
 
 const Course = ({ course }) => {
+  const total = course.parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
+
   return (
     <div>
       <h1>{course.name}</h1>
       {course.parts.map((exer) => {
-        return <p key={exer.id}>{exer.name}</p>;
+        return (
+          <p key={exer.id}>
+            <span>{exer.name}</span> <span>{exer.exercises}</span>
+          </p>
+        );
       })}
+      <strong>{`total of ${total} exercises`}</strong>
     </div>
   );
 };
