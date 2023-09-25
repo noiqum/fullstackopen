850214eb74d7e5e45da517a9c4b532ad27cf61cd
onur  coskun <onur.coskun@wunder.is>
2023-09-25 22:08:08 +0300

    exercise 2.9

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const newNameHandler = (event) => {
     event.preventDefault();
@@ -31,6 +37,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           name: <input value={newName} onChange={newNameHandler} />
@@ -43,13 +54,15 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
-        return (
-          <p key={person.name}>
-            <span>{person.name}</span> <span>{person.number}</span>
-          </p>
-        );
-      })}
+      {persons
+        .filter((elm) => elm.name.toLocaleLowerCase().includes(filter))
+        .map((person) => {
+          return (
+            <p key={person.name}>
+              <span>{person.name}</span> <span>{person.number}</span>
+            </p>
+          );
+        })}
     </div>
   );
 };
